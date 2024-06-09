38b7a34630e0d3a3aa4a3012da5dd1294c2d4dd7
Anish jha <anishjha805@gmail.com>
2024-06-09 11:02:19 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import Homebanner1 from "@/components/HomeBanner1/HomeBanner1";
+import HomeBanner2 from "@/components/HomeBanner2/HomeBanner2";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  <Homebanner1/>
+  <HomeBanner2/>
+    </main>
+  );
+}
